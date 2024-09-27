25df02ab161cfa09a86712385b271a9a87c22595
Вероника <@ASUS>
2024-09-27 22:08:50 +0300

    добавление модуля интерфейса и функций

---
+++ shellsort/functions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <time.h>
+
+using namespace std;
+
+int CheckInt();
+double CheckDouble();
+void PrintArray(vector<double>& arr);
