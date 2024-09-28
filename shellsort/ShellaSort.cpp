b0833cfecba9b06b4210e454461604bc856c03a4
Вероника <@ASUS>
2024-09-28 23:14:35 +0300

    исправлен warning

--- shellsort/ShellaSort.cpp
+++ shellsort/ShellaSort.cpp
@@ -15,6 +15,19 @@
 #include "functions.h"
 
 void ShellaSort::sort(vector<double>& arr) {
+	for (size_t step = arr.size() / 2; step > 0; step /= 2) { // Используем size_t
+		for (size_t i = step; i < arr.size(); i++) { // Используем size_t
+			double temp = arr[i];
+			size_t j; // Используем size_t
+			for (j = i; j >= step && arr[j - step] > temp; j -= step) {
+				arr[j] = arr[j - step];
+			}
+			arr[j] = temp;
+		}
+	}
+}
+
+/*void ShellaSort::sort(vector<double>& arr) {
 	for (int step = arr.size() / 2; step > 0; step /= 2) {
 		for (int i = step; i < arr.size(); i++) {
 			double temp = arr[i];
@@ -26,3 +39,4 @@ void ShellaSort::sort(vector<double>& arr) {
 		}
 	}
 }
+*/
